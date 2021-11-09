89a8cd18c51979efe09af5c9e70100aeaf94a7a8
Sherylee West <sherylee.west@mymona.uwi.edu>
2021-11-09 15:14:21 -0700

    Exercise1-3

--- world.php
+++ world.php
@@ -1,17 +1,20 @@
 <?php
+header('Access-Control-Allow-Origin: *'); 
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
+$country =$_GET['country'];
+$context =$_GET['context'];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries");
+if ($context=='none'){
+  $stmt = $conn->query("SELECT * FROM countries WHERE NAME LIKE '%$country%'");
+}else if($context=='cities'){
+  $stmt = $conn->query("SELECT cities.name,cities.district,cities.population FROM countries JOIN cities ON countries.code=cities.country_code WHERE countries.name LIKE '%$country%'");
 
+}
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+
